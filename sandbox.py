51541db29c40ed65803c755321589e23b95aea8b
ryancb4 <64450632+ryancb4@users.noreply.github.com>
2020-10-20 12:14:23 -0400

    Update sandbox.py
    got all of the internets here

--- sandbox.py
+++ sandbox.py
@@ -16,3 +16,9 @@ a_tots_acceptable_var_name_thats_so_very_descrptive_that_all_other_devs_will_kno
 
 if a_tots_acceptable_var_name_thats_so_very_descrptive_that_all_other_devs_will_know_exactly_what_its_for:
     print("Found that a_tots_acceptable_var_name_thats_so_very_descrptive_that_all_other_devs_will_know_exactly_what_its_for was true. Noice!")
+    
+
+best_data_base = []
+downloaded_internet = 1
+
+best_data_base.append(downloaded_internet)

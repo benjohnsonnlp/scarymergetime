8f8be341929da5729f22891fa3e18639e83ab64c
Ben <doc.ben.johnson@gmail.com>
2020-10-20 12:03:19 -0400

    go to town

---
+++ sandbox.py
@@ -0,0 +1,7 @@
+for i in range(1000):
+    print("if this is even, I have a thing here: {}".format("thing" if not i % 2 else ""))
+
+print("Sometimes... I get nervous... on airplanes")
+
+
+print(str([dog[0] for dog in ["charlie", "snoopy"]]))

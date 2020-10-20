db9a5ca17e1de24d0bc42761cd822dc2f8e29168
ktag0629 <kenta1@umbc.edu>
2020-10-20 12:07:01 -0400

    rando changes

--- sandbox.py
+++ sandbox.py
@@ -2,6 +2,14 @@ for i in range(1000):
     print("if this is even, I have a thing here: {}".format("thing" if not i % 2 else ""))
 
 print("Sometimes... I get nervous... on airplanes")
+print("Schooool appropriate stufff --- Kent")
 
 
+for i in range(1):
+    print("I just wanna say it once.")
+
+
+one = 1
+juan = 2
+print("one plus juan = ", one + juan)
 print(str([dog[0] for dog in ["charlie", "snoopy"]]))

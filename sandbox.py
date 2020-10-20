31cd542eedd195daf2ea6ca4aed390d6fc61eb31
MaksimEkin <meren1@umbc.edu>
2020-10-20 12:08:07 -0400

    Good stuff.

--- sandbox.py
+++ sandbox.py
@@ -5,3 +5,10 @@ print("Sometimes... I get nervous... on airplanes")
 
 
 print(str([dog[0] for dog in ["charlie", "snoopy"]]))
+
+
+# Below is some code
+good_stuff = {"stuff 1", "stuff 3", " stuff 2"}
+not_good_stuff = {" not stuff 5", "stuff 3", " not good stuff 2"}
+good_stuff |= not_good_stuff
+print(good_stuff)

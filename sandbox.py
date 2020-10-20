abe63a467760151b62655c1214358350d282c992
ryancb4 <64450632+ryancb4@users.noreply.github.com>
2020-10-20 12:10:42 -0400

    Update sandbox.py
    added a very important variable

--- sandbox.py
+++ sandbox.py
@@ -1,20 +1,6 @@
 for i in range(1000):
     print("if this is even, I have a thing here: {}".format("thing" if not i % 2 else ""))
 
-<<<<<<< HEAD
-print("Sometimes... I get nervous... on airplanes")
-print("Schooool appropriate stufff --- Kent")
-
-
-for i in range(1):
-    print("I just wanna say it once.")
-
-
-one = 1
-juan = 2
-print("one plus juan = ", one + juan)
-print(str([dog[0] for dog in ["charlie", "snoopy"]]))
-=======
 print("Sometimes... I get nervous when I see a spooky doggie wearing skelly suit :O")
 
 
@@ -24,4 +10,9 @@ print(str([dog[0] for dog in ["charlie", "snoopy"]]))
 
 print("Hello Friends!")
 print("Gotta make some origami real quick, be right back...")
->>>>>>> 8df605b43fa8dc996736e35c2146c9d25bc87074
+
+
+a_tots_acceptable_var_name_thats_so_very_descrptive_that_all_other_devs_will_know_exactly_what_its_for = true
+
+if a_tots_acceptable_var_name_thats_so_very_descrptive_that_all_other_devs_will_know_exactly_what_its_for:
+    print("Found that a_tots_acceptable_var_name_thats_so_very_descrptive_that_all_other_devs_will_know_exactly_what_its_for was true. Noice!")

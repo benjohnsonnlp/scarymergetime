8df605b43fa8dc996736e35c2146c9d25bc87074
ryancb4 <64450632+ryancb4@users.noreply.github.com>
2020-10-20 12:06:01 -0400

    Update sandbox.py
    Excellent!

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,12 @@
 for i in range(1000):
     print("if this is even, I have a thing here: {}".format("thing" if not i % 2 else ""))
 
-print("Sometimes... I get nervous... on airplanes")
+print("Sometimes... I get nervous when I see a spooky doggie wearing skelly suit :O")
 
 
 print(str([dog[0] for dog in ["charlie", "snoopy"]]))
+
+
+
+print("Hello Friends!")
+print("Gotta make some origami real quick, be right back...")
